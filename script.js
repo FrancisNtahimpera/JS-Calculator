5ffe9db5f63957932bf465b6a27383851ab31e44
Francisntahimpera <ntahimperafrancis@gmail.com>
2022-06-13 12:43:15 +0200

    css

---
+++ script.js
@@ -0,0 +1,26 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        $this.previousOperandTextElement = previousOperandTextElement;
+        $this.currentOperandTextElement = currentOperandTextElement;
+    }
+    clear() {}
+
+    delete() {}
+
+    appendNumber() {}
+
+    chooseOperations(operation) {}
+
+    compute() {}
+
+    updateDisplay() {}
+}
+
+
+const numberButton = document.querySelectorAll('[data-number]');
+const operationButton = document.querySelectorAll('[data-operation]');
+const equalButton = document.querySelectorAll('[data-equal]');
+const deleteButton = document.querySelectorAll('[data-delete]');
+const allClearButton = document.querySelectorAll('[data-all-clear]');
+const previousOperandButton = document.querySelectorAll('[data-previous-operand]');
+const currentOperandButton = document.querySelectorAll('[data-current-operand]');
